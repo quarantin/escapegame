9a449b5fb2b443d111536b0e1998d02af643b313
Corentin Delorme <codelorme@gmail.com>
2018-07-26 17:50:53 +0200

    Add 127.0.0.1 and localhost to ALLOWED_HOSTS for easier debug.

--- escapegame/settings.py
+++ escapegame/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'l7z2w=efd90^)1gi6a$u$^ohl&tnc=*aby*vr5z5)^-22^voh)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'raspberrypi.local' ]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'raspberrypi.local'
+]
 
 
 # Application definition
