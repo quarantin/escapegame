1a2f7909275fca7f104e89b89a1ea3c2dbc302ea
Corentin Delorme <codelorme@gmail.com>
2018-07-27 13:50:02 +0200

    Add video player to the model of 1001 nuits

--- mille_et_une_nuits/models.py
+++ mille_et_une_nuits/models.py
@@ -5,11 +5,15 @@ invalid_pin = -1
 default_duration_door = 10
 default_duration_led = 10
 default_video_path = '/opt/vc/src/hello_pi/hello_video/test.h264'
+default_video_player = 'omxplayer'
 
 class MilleEtUneNuitsSettings(SingletonModel):
 
 	# Path to welcome video
 	video_path = models.CharField(max_length=255, default=default_video_path)
+
+	# Video player name
+	video_player = models.CharField(max_length=255, default=default_video_player)
 
 	# Garden door (Jardin)
 	pin_door_garden = models.IntegerField(default=invalid_pin)
