f06e3e624e5fe577473192ad07f978f702fd4d9f
Corentin Delorme <codelorme@gmail.com>
2018-07-30 15:53:22 +0200

    Fix typo in db.sh + create players + create escapegames

--- db.sh
+++ db.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-rm -f db.sqlite
+# Clear database
+rm -f db.sqlite3
 
+# Create database
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py createsuperuser --user gamemater --email none@mail.com
+
+# Add video player "mplayer"
+python3 manage.py shell -c "from video.models import VideoPlayer; VideoPlayer(video_player='mplayer').save()"
+
+# Add video player "omxplayer"
+python3 manage.py shell -c "from video.models import VideoPlayer; VideoPlayer(video_player='omxplayer').save()"
+
+# Add escape game "Mille et une nuits"
+python3 manage.py shell -c "from escapegame.models import EscapeGame; EscapeGame(name='Mille et une nuits', video='test.h264').save()"
+
+# Add escape game "Stranger Things"
+python3 manage.py shell -c "from escapegame.models import EscapeGame; EscapeGame(name='Mille et une nuits', video='test.h264').save()"
+
+python3 manage.py createsuperuser --user gamemaster --email none@mail.com
