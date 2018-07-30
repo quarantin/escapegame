295e94d4c9e09be256091632e102d6c2345f35f1
Corentin Delorme <codelorme@gmail.com>
2018-07-30 16:43:40 +0200

    Delete useless views from stranger_things app

--- stranger_things/urls.py
+++ stranger_things/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='Index'),
-	path('video-player', views.video_player, name='Video Player'),
-	path('door-control', views.door_control, name='Door Control'),
-	path('light-control', views.light_control, name='Light Control'),
 ]
--- stranger_things/views.py
+++ stranger_things/views.py
@@ -7,15 +7,3 @@ from django.contrib.auth.decorators import login_required
 def index(request):
 	template = loader.get_template('stranger_things/index.html')
 	return HttpResponse(template.render({}, request))
-
-@login_required
-def video_player(request):
-	return HttpResponse('Video player view (stranger things)')
-
-@login_required
-def door_control(request):
-	return HttpResponse('Door control view (stranger things)')
-
-@login_required
-def light_control(request):
-	return HttpResponse('Light control view (stranger things)')
