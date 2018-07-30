84261559580ddcc60af24861fbd9272d70b38583
Corentin Delorme <codelorme@gmail.com>
2018-07-30 17:54:14 +0200

    Remove useless model from stranger_things app

--- stranger_things/admin.py
+++ stranger_things/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import StrangerThingsSettings
-
-admin.site.register(StrangerThingsSettings)
--- stranger_things/models.py
+++ stranger_things/models.py
@@ -1,27 +1,2 @@
 from django.db import models
 
-invalid_pin = -1
-default_duration_door = 10
-default_duration_led = 10
-
-class StrangerThingsSettings(models.Model):
-
-	# Dark door (Sombre)
-	pin_door_dark = models.IntegerField(default=invalid_pin)
-	pin_door_dark_duration = models.IntegerField(default=default_duration_door)
-
-	# Light door (Claire)
-	pin_door_cave = models.IntegerField(default=invalid_pin)
-	pin_door_cave_duration = models.IntegerField(default=default_duration_door)
-
-	# Wood door (Bois)
-	pin_door_wood = models.IntegerField(default=invalid_pin)
-	pin_door_wood_duration = models.IntegerField(default=default_duration_door)
-
-	# Cabin door (Cabane)
-	pin_door_cabin = models.IntegerField(default=invalid_pin)
-	pin_door_cabin_duration = models.IntegerField(default=default_duration_door)
-
-	# TODO LED PINs...
-	# pin_led_whatever = models.IntegerField(default=invalid_pin)
-	# pin_led_whatever_duration = models.IntegerField(default=default_duration_led)
