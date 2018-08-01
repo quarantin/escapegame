80bfe6f01078637f2f1191cf63d09351bda28443
Corentin Delorme <codelorme@gmail.com>
2018-08-01 14:21:57 +0200

    Make the setting VIDEO_PLAYER dynamic depending if running on pi or not

--- escapegame/settings.py
+++ escapegame/settings.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from django_admin_conf_vars.global_vars import config
 
-config.set('VIDEO_PLAYER', default='omxplayer')
+import os
 
+default_player = 'omxplayer'
+if not ' '.join(os.uname()).strip().endswith('armv7l'):
+	default_player = 'mpv'
+
+config.set('VIDEO_PLAYER', default=default_player)
