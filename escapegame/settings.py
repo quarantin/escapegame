7b2a239861c62e4b3387b361a2fd7c36102897aa
Corentin Delorme <codelorme@gmail.com>
2018-08-01 15:10:08 +0200

    Add global setting RUNNING_ON_PI

--- escapegame/settings.py
+++ escapegame/settings.py
@@ -4,9 +4,13 @@ from django_admin_conf_vars.global_vars import config
 
 import os
 
+running_on_pi = True
 default_player = 'omxplayer'
+
 if not ' '.join(os.uname()).strip().endswith('armv7l'):
+	running_on_pi = False
 	default_player = 'mpv'
 
+config.set('RUNNING_ON_PI', default=running_on_pi)
 config.set('VIDEO_PATH', default='/opt/vc/src/hello_pi/hello_video')
 config.set('VIDEO_PLAYER', default=default_player)
