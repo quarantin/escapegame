86dac7ab9a6365092bacedb95b3a41dc482958c7
Corentin Delorme <codelorme@gmail.com>
2018-08-02 11:38:15 +0200

    Move MEDIA_ROOT and MEDIA_URL to siteconfig/settings.py instead of the dynamic version at escapegame/settings.py (2)

--- escapegame/settings.py
+++ escapegame/settings.py
@@ -4,20 +4,20 @@ from django_admin_conf_vars.global_vars import config
 
 import os
 
-default_media_root = '/home/pi/media'
-default_media_url = '/media/'
+#default_media_root = '/home/pi/media'
+#default_media_url = '/media/'
 default_upload_path = 'uploads'
 default_video_path = '/opt/vc/src/hello_pi/hello_video'
 default_video_player = 'omxplayer'
 running_on_pi = True
 
 if not ' '.join(os.uname()).strip().endswith('armv7l'):
-	default_media_root = '/tmp'
+	#default_media_root = '/tmp'
 	default_video_player = 'mpv'
 	running_on_pi = False
 
-config.set('MEDIA_ROOT', default=default_media_root)
-config.set('MEDIA_URL', default=default_media_url)
+#config.set('MEDIA_ROOT', default=default_media_root)
+#config.set('MEDIA_URL', default=default_media_url)
 config.set('RUNNING_ON_PI', default=running_on_pi)
 config.set('UPLOAD_PATH', default=default_upload_path)
 config.set('VIDEO_PATH', default=default_video_path)
