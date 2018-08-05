6fcb7fb5051fb8cbef4a6b3e43bb01f02caeecf8
Corentin Delorme <codelorme@gmail.com>
2018-08-06 01:27:49 +0200

    Update script provision.sh: Add django-background-tasks module as a dependency

--- siteconfig/settings.py
+++ siteconfig/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'web',
     'escapegame',
     'corsheaders',
+    'background_task',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
