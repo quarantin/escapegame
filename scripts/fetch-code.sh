bbf019768b9e9a8f2660433a6290fa147271d1f0
Corentin Delorme <codelorme@gmail.com>
2018-08-05 12:57:13 +0200

    Update script fetch-code.sh

--- scripts/fetch-code.sh
+++ scripts/fetch-code.sh
@@ -6,7 +6,6 @@ BRANCH=master
 OUTPUT=$(git status --porcelain)
 MODIFIED=$(echo "${OUTPUT}" | grep '^ M ' | wc -l)
 if [ "${MODIFIED}" -gt "0" ]; then
-	echo "You have some local modifications, please commit or stash them first."
 	git status
 	exit
 fi
