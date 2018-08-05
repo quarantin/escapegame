90f2e0d15b83c97ddd7f560bbc4b898d2b869076
Corentin Delorme <codelorme@gmail.com>
2018-08-05 12:39:53 +0200

    Add script to fetch code, also check if there is local modifications before proceeding

---
+++ scripts/fetch-code.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ORIGIN=corentin
+BRANCH=master
+
+OUTPUT=$(git status --porcelain)
+echo $OUTPUT
+echo "======="
+MODIFIED=$(echo "${OUTPUT}" | grep '^ M ' | wc -l)
+if [ "${MODIFIED}" -gt "0" ]; then
+	echo "You have some local modifications, please commit or stash them first."
+	git status
+	exit
+fi
+
+git fetch "${ORIGIN}" && git pull "${ORIGIN}" "${BRANCH}"
