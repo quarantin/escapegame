706ff023b7d9f110171e5280b3fe896f9e355546
Corentin Delorme <codelorme@gmail.com>
2018-08-05 20:49:37 +0200

    Remove unused import is settings/urls.py

--- siteconfig/urls.py
+++ siteconfig/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.contrib.auth.views import LoginView
 #from django.views.generic.base import RedirectView
 
