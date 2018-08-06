0e8caf37707e44c79938e21c8566a1baeed8010f
Corentin Delorme <codelorme@gmail.com>
2018-08-06 10:21:26 +0200

    Add GNU screen to dependencies in script provision.sh + Setup .vimrc for pi user

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,10 +2,17 @@
 
 . $(dirname $0)/env.sh
 
-DEBIAN_PACKAGES=( 'sqlite3' 'vim' )
+DEBIAN_PACKAGES=( 'sqlite3' 'vim' 'screen' )
 
 PIP_PACKAGES=( 'django' 'django-cors-headers' 'django-background-tasks' 'django-constance[database]' )
 
 sudo apt-get install "${DEBIAN_PACKAGES[@]}"
 
 sudo -H ${PIP} install "${PIP_PACKAGES[@]}"
+
+if [ "$USER" = "pi" ]; then
+echo <<< EOF
+syntax on
+set tabstop=8 noexpandtab
+EOF >> ~/.vimrc
+fi
