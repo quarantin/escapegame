f8fed74bd7c5ffda5445b884eb4da9055c3eef70
Corentin Delorme <codelorme@gmail.com>
2018-08-06 13:22:13 +0200

    Do not add the background task if it's already defined

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from escapegame.models import RaspberryPi, RemoteChallengePin
+from background_task.models import Task
 from . import tasks
 
 from constance import config
@@ -7,21 +8,26 @@ from constance import config
 import socket
 
 try:
-	myself = RaspberryPi.objects.get(hostname='%s.local' % socket.gethostname())
-	if myself:
-		remote_pins = RemoteChallengePin.objects.filter(raspberrypi=myself)
-		for remote_pin in remote_pins:
-			tasks.poll_gpio(remote_pin.pin_number)
+	task = Task.objects.get(task_name='api.tasks.poll_gpio')
+	print("Not adding background task %s, already present in db" % task.task_name)
+except:
 
-except Exception as err:
+	try:
+		myself = RaspberryPi.objects.get(hostname='%s.local' % socket.gethostname())
+		if myself:
+			remote_pins = RemoteChallengePin.objects.filter(raspberrypi=myself)
+			for remote_pin in remote_pins:
+				tasks.poll_gpio(remote_pin.pin_number)
 
-	err = str(err)
+	except Exception as err:
 
-	if err.startswith('no such table: escapegame_raspberrypi'):
-		pass
+		err = str(err)
 
-	elif err.startswith('RaspberryPi matching query does not exist') and config.IS_MASTER:
-		pass
+		if err.startswith('no such table: escapegame_raspberrypi'):
+			pass
 
-	else:
-		print("Adding background tasks failed! (Error: %s)" % err)
+		elif err.startswith('RaspberryPi matching query does not exist') and config.IS_MASTER:
+			pass
+
+		else:
+			print("Adding background tasks failed! (Error: %s)" % err)
