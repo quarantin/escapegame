dab3802ce9de3f4f8e1a895cfcaf2ed11417163f
Corentin Delorme <codelorme@gmail.com>
2018-08-06 15:01:03 +0200

    Log django service output to file

--- scripts/django-background-service.sh
+++ scripts/django-background-service.sh
@@ -3,12 +3,14 @@
 . $(dirname $0)/env.sh
 
 SLEEP=1
+LOGFILE='django-tasks.log'
 
 cd "${ROOTDIR}"
 
+echo -n > "${LOGFILE}"
+
 while true; do
-	"${PYTHON}" manage.py process_tasks
-	echo "${PYTHON} died! Restarting script in ${SLEEP}"
+	"${PYTHON}" manage.py process_tasks                  2>&1 >> "${LOGFILE}"
+	echo "${PYTHON} died! Restarting django background tasks in ${SLEEP}" 2>&1 >> "${LOGFILE}"
 	sleep "${SLEEP}"
 done
-
--- scripts/django-service.sh
+++ scripts/django-service.sh
@@ -4,12 +4,14 @@
 
 SLEEP=1
 PORT=80
+LOGFILE='django.log'
 
 cd "${ROOTDIR}"
 
+echo -n > "${LOGFILE}"
+
 while true; do
-	"${PYTHON}" manage.py runserver "${HOSTNAME}:${PORT}"
-	echo "${PYTHON} died! Restarting django in ${SLEEP}"
+	"${PYTHON}" manage.py runserver "${HOSTNAME}:${PORT}" 2>&1 >> "${LOGFILE}"
+	echo "${PYTHON} died! Restarting django in ${SLEEP}"  2>&1 >> "${LOGFILE}"
 	sleep "${SLEEP}"
 done
-
