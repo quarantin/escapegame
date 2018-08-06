aae0f8400d98822e2f5ded77c51fa2cbe09e7ebf
Corentin Delorme <codelorme@gmail.com>
2018-08-06 13:12:17 +0200

    Execute background tasks asynchronously

--- api/admin.py
+++ api/admin.py
@@ -11,7 +11,7 @@ try:
 	if myself:
 		remote_pins = RemoteChallengePin.objects.filter(raspberrypi=myself)
 		for remote_pin in remote_pins:
-			tasks.poll_gpio.now(remote_pin.pin_number)
+			tasks.poll_gpio(remote_pin.pin_number)
 
 except Exception as err:
 
