f2bb155f5f73ad0a57a2cd750f9fa6d9f48cfad5
Corentin Delorme <codelorme@gmail.com>
2018-08-06 19:56:52 +0200

    Use custom logger for each each process handling remote pins

--- escapegame/apps.py
+++ escapegame/apps.py
@@ -5,6 +5,8 @@ from django.apps import AppConfig
 import logging
 
 class EscapegameConfig(AppConfig):
-    name = 'escapegame'
-    logger = logging.getLogger(name)
-    taskLogger = logging.getLogger('%s.tasks' % name)
+	name = 'escapegame'
+	logger = logging.getLogger(name)
+
+	def taskLogger(self, pin):
+		return logging.getLogger('%s.tasks.poll.gpio.%d' % (self.name, pin))
