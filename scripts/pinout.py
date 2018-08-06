f0ed62bab496b0974e2e9f60f2d351dc63768e6c
Corentin Delorme <codelorme@gmail.com>
2018-08-06 15:32:37 +0200

    Add script pinout.py to show current GPIO state on a raspberry pi

---
+++ scripts/pinout.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+
+# See:
+# https://sourceforge.net/p/raspberry-gpio-python/wiki/Checking%20function%20of%20GPIO%20channels/
+
+FUNCTIONS = {
+	GPIO.IN: 'GPIO.IN',
+	GPIO.OUT: 'GPIO.OUT',
+	GPIO.SPI: 'GPIO.SPI',
+	GPIO.I2C: 'GPIO.I2C',
+	GPIO.HARD_PWM: 'GPIO.HARD_PWM',
+	GPIO.SERIAL: 'GPIO.SERIAL',
+	GPIO.UNKNOWN: 'GPIO.UNKNOWN',
+}
+
+GPIO.setmode(GPIO.BOARD)
+for pin in range(0, 32):
+
+	try:
+		func = GPIO.gpio_function(pin)
+		if func in FUNCTIONS:
+			func = FUNCTIONS[func]
+
+		print("PIN %d = %s" % (pin, func))
+
+	except Exception as err:
+		#print('Error: %s' % err)
+		print("PIN %d is not a GPIO" % pin)
+		pass
+
