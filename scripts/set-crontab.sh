a345cc4c451b226288fb656b53e668d06a506845
Corentin Delorme <codelorme@gmail.com>
2018-08-06 13:53:44 +0200

    Update script set-crontab.sh to also install django-background-service.sh script

--- scripts/set-crontab.sh
+++ scripts/set-crontab.sh
@@ -2,23 +2,27 @@
 
 . $(dirname $0)/env.sh
 
-ENTRY=$(crontab -l 2>&1 | grep django-service)
+ENTRY=$(crontab -l 2>&1 | grep django-background-service)
 if [ -z "$ENTRY" ]; then
 
 	CRONTAB=$(mktemp)
 
-	echo "@reboot ${ROOTDIR}/scripts/django-service.sh &" > "${CRONTAB}"
+	echo "@reboot ${ROOTDIR}/scripts/django-service.sh &"            >> "${CRONTAB}"
+	echo "@reboot ${ROOTDIR}/scripts/django-background-service.sh &" >> "${CRONTAB}"
+
 	crontab "${CRONTAB}"
 	STATUS=$?
 	rm -f "${CRONTAB}"
 	if [ "$STATUS" = "0" ]; then
 		crontab -l
-		echo "[ + ] django-service.sh installed successfully."
+		echo "[ + ] Installed successfully django-service.sh"
+		echo "[ + ] Installed successfully django-background-service.sh"
 	else
 		echo "ERROR: failed to install django-service.sh!"
 	fi
 
 else
 	crontab -l
-	echo "[ + ] django-service.sh is already configured."
+	echo "[ + ] Already configured django-service.sh"
+	echo "[ + ] Already configured django-background-service.sh"
 fi
