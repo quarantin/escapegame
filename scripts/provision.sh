b8bd19f534453731de10e3942dff6181eddf9d78
Corentin Delorme <codelorme@gmail.com>
2018-08-06 20:55:58 +0200

    Sets timezone in script provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,10 +6,13 @@ DEBIAN_PACKAGES=( 'sqlite3' 'vim' 'screen' 'mysql-server' )
 
 PIP_PACKAGES=( 'django' 'django-cors-headers' 'django-background-tasks' 'django-constance[database]' 'mysqlclient' )
 
+# Install Debian packages
 sudo apt-get install "${DEBIAN_PACKAGES[@]}"
 
+# Install pip packages
 sudo -H ${PIP} install "${PIP_PACKAGES[@]}"
 
+# Creates default ~/.vimrc
 if [ "$USER" = "pi" ]; then
 cat << EOF > ~/.vimrc
 syntax on
@@ -19,4 +22,8 @@ set tabstop=4 noexpandtab
 EOF
 fi
 
+# Configure timezone
+sudo timedatectl set-timezone Europe/Paris
+
+# Hide GNU screen startup message
 sudo sed -i 's/^#startup_message off$/startup_message off/' /etc/screenrc
