f248998eb99b1a66aaa26c022df90bef934af320
Corentin Delorme <codelorme@gmail.com>
2018-08-06 15:42:37 +0200

    Update script test-gpio-out.py to use pin 24 by default because it is configured with GPIO.OUT at boot time

--- scripts/test-gpio-out.py
+++ scripts/test-gpio-out.py
@@ -3,16 +3,31 @@
 import sys
 import RPi.GPIO as GPIO
 
-pin = 12
+pin = 24
 if len(sys.argv) > 1:
 	pin = int(sys.argv[1])
 
-print("Configuring pin %d for output" % pin)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pin, GPIO.OUT)
+print("Please ensure pin %d is wired directly to the challenge pin you want to test." % pin)
+input("[ Press enter to continue when ready ]")
 
-for i in range(0, 10):
+GPIO.setmode(GPIO.BOARD)
+
+print("Configuring pin %d for output" % pin)
+
+gpio_type = GPIO.gpio_function(pin)
+if gpio_type != GPIO.OUT:
+	GPIO.setup(pin, GPIO.OUT)
+
+i = -1
+
+while True:
+
+	i += 1
+
 	state = ((i % 2) == 0) and GPIO.HIGH or GPIO.LOW
+
 	print("Writing state: %s" % state)
+
 	GPIO.output(pin, state)
-	input("Press enter for next output...")
+
+	input("[ Press enter for next output ]")
