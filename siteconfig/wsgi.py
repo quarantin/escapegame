2d3348d7764a1b84878371df4bd42186a7178805
Corentin Delorme <codelorme@gmail.com>
2018-08-07 00:31:54 +0200

    Fix invalid module name in siteconfig/wsgi.py. This was a remnant from the first install where the config folder was named escapegame instead of siteconfig

--- siteconfig/wsgi.py
+++ siteconfig/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "escapegame.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteconfig.settings")
 
 application = get_wsgi_application()
