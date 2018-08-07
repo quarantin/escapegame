b47e44435da86328909cb351c37ffea3b6446701
Corentin Delorme <codelorme@gmail.com>
2018-08-07 03:03:57 +0200

    Update crontab to start uwsgi

--- scripts/set-crontab.sh
+++ scripts/set-crontab.sh
@@ -4,16 +4,16 @@
 
 CRONTAB=$(mktemp)
 
-echo "@reboot ${ROOTDIR}/scripts/django-service.sh" >> "${CRONTAB}"
+echo "@reboot sudo /usr/bin/uwsgi --ini /etc/uwsgi/apps-enabled/escapegame.ini" >> "${CRONTAB}"
 echo "@reboot ${ROOTDIR}/scripts/django-background-service.sh" >> "${CRONTAB}"
 
 crontab "${CRONTAB}"
 STATUS=$?
 rm -f "${CRONTAB}"
-if [ "$STATUS" = "0" ]; then
+if [ "${STATUS}" = "0" ]; then
 	crontab -l
-	echo "[ + ] Installed successfully django-service.sh"
+	echo "[ + ] Installed successfully uwsgi"
 	echo "[ + ] Installed successfully django-background-service.sh"
 else
-	echo "ERROR: failed to install django-service.sh!"
+	echo "ERROR: failed to install crontab!"
 fi
