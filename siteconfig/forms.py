221622cacd1c093af1858e5aa475c53926389073
Corentin Delorme <codelorme@gmail.com>
2018-08-08 23:18:59 +0200

    Add JSON import/export

---
+++ siteconfig/forms.py
@@ -0,0 +1,102 @@
+from django import forms
+
+from escapegame.models import *
+
+import json
+
+from datetime import datetime
+
+from collections import OrderedDict
+
+def get_sorted_query_set(queryset):
+
+	result = []
+
+	for obj in queryset:
+		objdict = OrderedDict()
+		for key in sorted(obj.keys()):
+			objdict[key] = obj[key]
+
+		result.append(objdict)
+
+	return result
+	
+class JsonImportForm(forms.Form):
+	json_configuration = forms.FileField(label='Upload JSON')
+
+	def json_import(self, json_configuration):
+
+		databytes = bytearray()
+		for chunk in json_configuration.chunks():
+			databytes += chunk
+
+		config = json.loads(databytes.decode('utf-8').strip())
+		# TODO write config to database
+
+class JsonExportForm(forms.Form):
+	indent = forms.BooleanField(label='Indent JSON', required=False)
+	export_date = forms.BooleanField(label='Export date', required=False)
+	software_version = forms.BooleanField(label='Software version', required=False)
+	escapegames = forms.BooleanField(label='Escape games', required=False)
+	raspberry_pis = forms.BooleanField(label='Raspberry Pis', required=False)
+	remote_challenge_pins = forms.BooleanField(label='Remote challenge pins', required=False)
+	remote_door_pins = forms.BooleanField(label='Remote door pins', required=False)
+	remote_led_pins = forms.BooleanField(label='Remote LED pins', required=False)
+	images = forms.BooleanField(label='Images', required=False)
+	videos = forms.BooleanField(label='Videos', required=False)
+
+	def json_export(self, post):
+
+		now = datetime.now()
+
+		config = OrderedDict()
+
+		# Check if we should beautify JSON
+		config['indent'] = False
+		if post.get('indent'):
+			config['indent'] = True
+
+		# Export the date of current export
+		if post.get('export_date'):
+			config['export_date'] = now.strftime('%Y-%m-%d %H:%M:%S')
+
+		# Export the software version
+		if post.get('software_version'):
+			config['software_version'] = libraspi.git_version()[:8]
+
+		# Setting the filename
+		config['filename'] = 'escapegame-config-%s.json' % now.strftime('%Y-%m-%d_%H-%M-%S')
+
+		# Export escape games, rooms, and challenges,
+		if post.get('escapegames'):
+			config['escapegames'] = get_sorted_query_set(EscapeGame.objects.all().order_by('id').values())
+			for escapegame in config['escapegames']:
+				escapegame['rooms'] = get_sorted_query_set(EscapeGameRoom.objects.filter(escapegame=escapegame['id']).order_by('id').values())
+				for room in escapegame['rooms']:
+					room['challenges'] = get_sorted_query_set(EscapeGameChallenge.objects.filter(room=room['id']).order_by('id').values())
+
+		# Export Raspberry Pis
+		if post.get('raspberry_pis'):
+			config['raspberry_pis'] = [ raspi for raspi in RaspberryPi.objects.all().order_by('id').values() ]
+
+		# Export remote challenge pins
+		if post.get('remote_challenge_pins'):
+			config['remote_challenge_pins'] = [ pin for pin in RemoteChallengePin.objects.all().order_by('id').values() ]
+
+		# Export the remote door pins
+		if post.get('remote_door_pins'):
+			config['remote_door_pins'] = [ pin for pin in RemoteDoorPin.objects.all().order_by('id').values() ]
+
+		# Export the remote LED pins
+		if post.get('remote_led_pins'):
+			config['remote_led_pins'] = [ pin for pin in RemoteLedPin.objects.all().order_by('id').values() ]
+
+		# Export images
+		if post.get('images'):
+			config['images'] = [ image for image in Image.objects.all().order_by('id').values() ]
+
+		# Export videos
+		if post.get('videos'):
+			config['videos'] = [ video for video in Video.objects.all().order_by('id').values() ]
+
+		return config
---
+++ siteconfig/views.py
@@ -0,0 +1,67 @@
+from django.template import loader
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseServerError
+
+from siteconfig.forms import JsonImportForm, JsonExportForm
+
+import json
+
+@csrf_exempt
+def import_view(request):
+
+	try:
+		if request.method == 'POST':
+			form = JsonImportForm(request.POST, request.FILES)
+			if form.is_valid():
+				form.json_import(request.FILES['json_configuration'])
+				messages.success("Successfully uploaded file '%s'" % request.FILES['json_configuration'])
+
+		else:
+			form = JsonImportForm()
+
+		context = {
+			'form': form,
+		}
+
+		template = loader.get_template('siteconfig/import.html')
+		return HttpResponse(template.render(context, request))
+
+	except Exception as err:
+		return HttpResponseServerError('Error: %s' % err)
+
+import traceback
+
+@csrf_exempt
+def export_view(request):
+
+	try:
+		if request.method == 'POST':
+			form = JsonExportForm(request.POST)
+			if form.is_valid():
+
+				jsonconfig = form.json_export(request.POST)
+
+				filename = jsonconfig.pop('filename', 'escapegame-config.json')
+				indent = jsonconfig.pop('indent', None) or None
+				if indent:
+					indent = 4
+
+				jsonstring = json.dumps(jsonconfig, indent=indent)
+				response = HttpResponse(jsonstring, content_type='application/json')
+				response['Content-Disposition'] = 'attachment; filename=%s' % filename
+				response['Content-Length'] = len(jsonstring)
+				return response
+
+		else:
+			form = JsonExportForm()
+
+		context = {
+			'form': form,
+		}
+
+		template = loader.get_template('siteconfig/export.html')
+		return HttpResponse(template.render(context, request))
+
+	except Exception as err:
+		return HttpResponseServerError('Error: %s' % traceback.format_exc())
