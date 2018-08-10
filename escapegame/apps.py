5a4ede10e7abc719498dd3280a74ec014d81de13
Corentin Delorme <codelorme@gmail.com>
2018-08-10 21:51:25 +0200

    Add custom logger for debug

--- escapegame/apps.py
+++ escapegame/apps.py
@@ -4,19 +4,17 @@ from django.apps import AppConfig
 
 import logging
 
-def print_error(message):
-	print('ERROR: %s' % message)
+class MyLogger:
+	def error(message):
+		print('ERROR: %s' % message)
 
-def print_info(message):
-	print('INFO: %s' % message)
+	def info(message):
+		print('INFO: %s' % message)
 
 class EscapegameConfig(AppConfig):
 	name = 'escapegame'
 	#logger = logging.getLogger(name)
-	logger = {
-		'error': print_error,
-		'info': print_info,
-	}
+	logger = MyLogger()
 
 	def taskLogger(pin):
 		return logging.getLogger('%s.tasks.poll.gpio.%d' % (EscapegameConfig.name, pin))
