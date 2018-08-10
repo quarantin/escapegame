ea8f5ff6e95e8428a41640ec35f8130f66dae416
Corentin Delorme <codelorme@gmail.com>
2018-08-10 21:23:00 +0200

    Temporarily disable logging subsystem

--- escapegame/apps.py
+++ escapegame/apps.py
@@ -4,9 +4,19 @@ from django.apps import AppConfig
 
 import logging
 
+def print_error(message):
+	print('ERROR: %s' % message)
+
+def print_info(message):
+	print('INFO: %s' % message)
+
 class EscapegameConfig(AppConfig):
 	name = 'escapegame'
-	logger = logging.getLogger(name)
+	#logger = logging.getLogger(name)
+	logger = {
+		'error': print_error,
+		'info': print_info,
+	}
 
 	def taskLogger(pin):
 		return logging.getLogger('%s.tasks.poll.gpio.%d' % (EscapegameConfig.name, pin))
