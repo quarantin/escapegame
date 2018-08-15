41a2bd02b260bce60c8af85450fcd53a335f8b88
Corentin Delorme <codelorme@gmail.com>
2018-08-15 11:45:46 +0200

    Update script runserver.sh: Stop nginx before running python manage.py runserver

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -6,6 +6,8 @@ SLEEP=1
 
 cd "${ROOTDIR}"
 
+sudo /etc/init.d/nginx stop
+
 while true; do
 	"${PYTHON}" manage.py runserver $HOSTNAME:80
 	echo "${PYTHON} died! Restarting django background tasks in ${SLEEP} second(s)"
