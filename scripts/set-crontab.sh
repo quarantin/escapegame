a727de400911e322a352a1f45b65f624659f69d1
Corentin Delorme <codelorme@gmail.com>
2018-08-27 22:14:28 +0200

    Update script set-crontab.sh: Also install websocket processes

--- scripts/set-crontab.sh
+++ scripts/set-crontab.sh
@@ -9,11 +9,24 @@ APPS=(
 
 CRONTAB=$(mktemp)
 
+
+# UWSGI instances for django and websockets
+
 for APP in "${APPS[@]}"; do
 	echo "@reboot sudo /usr/bin/uwsgi --ini /etc/uwsgi/apps-enabled/uwsgi.ini:${APP}" >> "${CRONTAB}"
 done
 
+
+# Django background tasks
+
 echo "@reboot ${ROOTDIR}/scripts/django-background-service.sh" >> "${CRONTAB}"
+
+
+# Django websocket processes
+
+echo "@reboot cd ${ROOTDIR} && ${PYTHON} manage.py websocket-1001-nuits"      >> "${CRONTAB}"
+echo "@reboot cd ${ROOTDIR} && ${PYTHON} manage.py websocket-stranger-things" >> "${CRONTAB}"
+
 
 crontab "${CRONTAB}"
 STATUS=$?
@@ -23,6 +36,8 @@ if [ "${STATUS}" = "0" ]; then
 	echo "[ + ] Installed successfully uwsgi for django"
 	echo "[ + ] Installed successfully uwsgi for websockets"
 	echo "[ + ] Installed successfully django-background-service.sh"
+	echo "[ + ] Installed successfully websocket-1001-nuits"
+	echo "[ + ] Installed successfully websocket-stranger-things"
 else
 	echo "ERROR: failed to install crontab!"
 fi
