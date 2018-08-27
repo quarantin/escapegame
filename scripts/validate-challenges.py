5a2e1715f091cd348076fe48e83d074056e9a16c
Corentin Delorme <codelorme@gmail.com>
2018-08-28 00:50:47 +0200

    Update script validate-challenges.py: Add remaining URLs.

--- scripts/validate-challenges.py
+++ scripts/validate-challenges.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
+import time
+
 import requests
+
 
 urls = [
 	'http://escapegame.local/web/les-1001-nuits/sas-les-1001-nuits/debut-du-jeu/validate/',
 	'http://escapegame.local/web/les-1001-nuits/la-fontaine/la-fontaine/validate/',
 	'http://escapegame.local/web/les-1001-nuits/la-fontaine/les-dalles/validate/',
+	'http://escapegame.local/web/les-1001-nuits/la-caverne/le-marchand/validate/',
+	'http://escapegame.local/web/les-1001-nuits/la-caverne/le-lanceur-de-couteaux/validate/',
+	'http://escapegame.local/web/les-1001-nuits/la-caverne/le-charmeur-de-serpents/validate/',
+	'http://escapegame.local/web/les-1001-nuits/la-caverne/le-fakir/validate/',
+	'http://escapegame.local/web/les-1001-nuits/la-lampe/la-lampe/validate/',
 ]
 
-for url in urls:
+input('Press enter to start validating the challenges')
 
-	input('Press enter to fetch URL:\n%s\n' % url)
+for url in urls:
+	time.sleep(2)
 	requests.get(url)
