50669401c0fe3c2777b15b15d1daf30b71cfc0ed
Corentin Delorme <codelorme@gmail.com>
2018-08-28 03:33:25 +0200

    Remove unused scripts related to video

--- scripts/video-pause.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. $(dirname $0)/env.sh
-
-mkfifo $PLAYER_FIFO 2> /dev/null
-
-echo pause > $PLAYER_FIFO
--- scripts/video-play-omx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. $(dirname $0)/env.sh
-
-mkfifo $PLAYER_FIFO 2> /dev/null
-
-cat $PLAYER_FIFO | omxplayer /opt/vc/src/hello_pi/hello_video/test.h264
