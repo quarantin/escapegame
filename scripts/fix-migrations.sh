1572acee6dfe857f937752e8caafe944f011e11d
Corentin Delorme <codelorme@gmail.com>
2018-08-31 15:26:08 +0200

    Update script fix-migrations.sh: Improvements

--- scripts/fix-migrations.sh
+++ scripts/fix-migrations.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 for F in */migrations/0*.py; do
+	echo -n "Fixing $F..."
 	DIFF=$(git diff $F)
-	COUNT=$(echo $DIFF | wc -l)
-	if [ "$COUNT" -eq "10" ]; then
-		git checkout $F
-	else
-		ADDS=$(echo $DIFF | grep '^+[^+]' | sed 's/^+//' | tail -n +2)
-		DELS=$(echo $DIFF | grep '^-[^-]' | sed 's/^-//' | tail -n +2)
+	COUNT=$(git diff $F | wc -l)
 
-		if [ "$ADDS" == "$DELS" ]; then
-			git checkout $F
-		fi
+	if [ "$COUNT" -eq "10" ] || [ "$COUNT" -eq "20" ]; then
+		git checkout $F
+		echo ' RESET'
+
+	elif [ "$COUNT" -ne "0" ]; then
+		echo ' MODIFIED'
+	#	ADDS=$(echo $DIFF | grep '^+[^+]' | sed 's/^+//' | tail -n +2)
+	#	DELS=$(echo $DIFF | grep '^-[^-]' | sed 's/^-//' | tail -n +2)
+	#
+	#	if [ "$ADDS" == "$DELS" ]; then
+	#		git checkout $F
+	#	fi
+	else
+		echo ' NOT TOUCHED'
 	fi
 done
