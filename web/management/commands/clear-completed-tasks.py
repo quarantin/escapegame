7d8f829f0b3f3d76454c8b5aaed9640cd80c2357
Corentin Delorme <codelorme@gmail.com>
2018-09-02 15:51:25 +0200

    Update managment command clear-completed-tasks: Surround method body with try/except

--- web/management/commands/clear-completed-tasks.py
+++ web/management/commands/clear-completed-tasks.py
@@ -7,5 +7,8 @@ class Command(BaseCommand):
 	help = 'Clear completed background tasks from database'
 
 	def handle(self, *args, **options):
-		from background_task.models_completed import CompletedTask
-		CompletedTask.objects.all().delete()
+		try:
+			from background_task.models_completed import CompletedTask
+			CompletedTask.objects.all().delete()
+		except CompletedTask.DoesNotExist:
+			pass
