19a8ebf027b6bb56b6557b511d7337091f73ba94
Corentin Delorme <codelorme@gmail.com>
2018-09-02 10:47:28 +0200

    Update module api: Refactor background task poll_gpio()

--- api/tasks.py
+++ api/tasks.py
@@ -2,7 +2,8 @@ from background_task import background
 
 from constance import config
 
-from escapegame.models import *
+from escapegame.models import EscapeGameChallenge
+from controllers.models import RaspberryPi, RemoteChallengePin
 
 import time
 
@@ -10,21 +11,24 @@ import time
 @background(schedule=0)
 def poll_gpio(pin):
 
-	myself = RaspberryPi.objects.get(hostname=config.HOSTNAME)
+	method = 'api.tasks.poll_gpio(%d)' % pin
+
+	myself = RaspberryPi.get_myself()
 	if not myself:
-		print("Could not find matching Raspberry Pi: %s" % config.HOSTNAME)
+		print('[%s] Could not find matching Raspberry Pi: %s' % (method, config.HOSTNAME))
 		return
 
 	challenges = EscapeGameChallenge.objects.filter(challenge_pin=pin)
 	remote_pin = RemoteChallengePin.objects.get(raspberrypi=myself, challenge__in=challenges)
 	if not remote_pin:
-		print("Could not find matching remote challenge pin: %d on Raspberry Pi: %s" % (pin, config.HOSTNAME))
+		print('[%s] Could not find matching remote challenge pin: %d on Raspberry Pi: %s' % (method, pin, config.HOSTNAME))
 		return
 
 	url_callback_reset = remote_pin.url_callback_reset
 	url_callback_validate = remote_pin.url_callback_validate
 
-	print("Polling for GPIO pin %d" % pin)
+	print('[%s] Polling for GPIO pin %d' % (method, pin))
+
 	while True:
 
 		try:
@@ -38,10 +42,16 @@ def poll_gpio(pin):
 
 			callback_url = (status == 0 and url_callback_reset or url_callback_validate)
 
-			print("Performing request GET %s" % callback_url)
+			print('[%s] Performing request GET %s' % (method, callback_url))
 			libraspi.do_get(callback_url)
 
 		except Exception as err:
-			print(err)
+			print('[%s] Error: %s' % err)
 
-		time.sleep(1)
+		try:
+			# time.sleep() can raise some exceptions on some architectures,
+			# so we call it from inside a try/except block just in case.
+			time.sleep(1)
+
+		except Exception as err:
+			print('[%s] Error: %s' % err)
