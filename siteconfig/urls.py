12c611c8e231007122ae9f8e395172f35767f2b7
Corentin Delorme <codelorme@gmail.com>
2018-09-09 02:31:08 +0200

    Update siteconfig urls: Remove useless imports

--- siteconfig/urls.py
+++ siteconfig/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.urls import include, path
-from django.contrib.auth.views import LoginView
-from django.views.generic.base import RedirectView
 
 from escapegame import admin as myadmin
 
@@ -52,5 +49,4 @@ urlpatterns += i18n_patterns(
 
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-	#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += staticfiles_urlpatterns()
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
