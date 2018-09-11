95a62efcc1bec2615395367ed41bca69374145ad
Corentin Delorme <codelorme@gmail.com>
2018-09-11 12:28:17 +0200

    Fix signal handlers

--- escapegame/signals/save.py
+++ escapegame/signals/save.py
@@ -9,15 +9,15 @@ from escapegame.libraspi import notify_frontend
 
 @receiver(post_save, sender=ChallengeGPIO)
 def save_challenge_gpio(sender, instance, created, **kwargs):
-	if instance.parent is not None:
-		notify_frontend(instance.parent)
+	if instance.challenge is not None:
+		notify_frontend(instance.challenge.room.game)
 
 @receiver(post_save, sender=CubeGPIO)
 def save_cube_gpio(sender, instance, created, **kwargs):
-	if instance.parent is not None:
-		notify_frontend(instance.parent)
+	if instance.challenge is not None:
+		notify_frontend(instance.challenge.room.game)
 
 @receiver(post_save, sender=DoorGPIO)
 def save_door_gpio(sender, instance, created, **kwargs):
-	if instance.parent is not None:
-		notify_frontend(instance.parent)
+	if instance.game is not None:
+		notify_frontend(instance.game)
