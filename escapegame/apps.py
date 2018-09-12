8e0740041c989967baf66369299d49a90fa9dce6
Corentin Delorme <codelorme@gmail.com>
2018-09-12 15:01:29 +0200

    Update escapegame apps: Update redis tasks when django is starting

--- escapegame/apps.py
+++ escapegame/apps.py
@@ -11,10 +11,14 @@ class EscapegameConfig(AppConfig):
 	logger = logging.getLogger(name)
 
 	def ready(self):
+		from siteconfig.settings import IS_MASTER
 		from multimedia import tasks as ext_tasks
 		from . import tasks as int_tasks
 		from .signals import save
 		from .signals import constance
+
+		if IS_MASTER:
+			self.updateRedisTasks()
 
 		db_tables = connection.introspection.table_names()
 
@@ -23,6 +27,36 @@ class EscapegameConfig(AppConfig):
 
 		if 'multimedia_video' in db_tables:
 			ext_tasks.setup_background_tasks()
+
+	def updateRedisTasks(self):
+		from siteconfig.settings import REDIS_HOST
+		from controllers.models import ChallengeGPIO
+		import redis
+		import json
+
+		client = redis.Redis(host=REDIS_HOST)
+
+		tasks = {}
+
+		gpios = ChallengeGPIO.objects.all()
+		for gpio in gpios:
+
+			host = gpio.controller.hostname
+			if host not in tasks:
+				tasks[host] = {}
+				tasks[host]['challenges'] = {}
+
+			tasks[host]['challenges']['reset_pin'] = gpio.reset_pin
+			tasks[host]['challenges']['action_pin'] = gpio.action_pin
+
+		for host in tasks:
+
+			key = 'tasks:%s' % host
+			val = json.dumps(tasks[host])
+
+			print("Updating redis tasks (%s, %s)..." % (key, val))
+
+			client.set(key, val))
 
 	def taskLogger(pin):
 		return logging.getLogger('%s.tasks.poll.gpio.%d' % (EscapegameConfig.name, pin))
