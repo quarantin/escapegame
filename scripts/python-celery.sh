1677d3a04f8b9f066ad894e6e3fdafb1de776555
Corentin Delorme <codelorme@gmail.com>
2018-09-12 21:39:31 +0200

    Update script python-celery.sh: Fix celery command line and create folder just in case.

--- scripts/python-celery.sh
+++ scripts/python-celery.sh
@@ -6,12 +6,15 @@ SLEEP=1
 
 GID=${UID}
 LOG_FILE=/var/log/celery.log
-STATE_FILE=/var/run/celery/worker.state
+STATE_DIR=/var/run/celery
+STATE_FILE=${STATE_DIR}/worker.state
 
 cd ${ROOTDIR}
 
 while true; do
-	sudo celery -A siteconfig --uid ${UID} --gid ${GID} --logfile ${LOG_FILE} --statedb ${STATE_FILE} worker
+	sudo mkdir -p ${STATE_DIR}
+	sudo chown ${UID}:${GID} ${STATE_DIR}
+	sudo celery -A siteconfig -b redis://${MASTER_HOSTNAME} worker --statedb ${STATE_FILE} --uid ${UID} --gid ${GID} --logfile ${LOG_FILE}
 	echo "celery died! Restarting celery in ${SLEEP} second(s)"
 	sleep ${SLEEP}
 done
