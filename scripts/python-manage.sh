2b461b1aa73950e0b646c791aeb9279ccebfadbb
Corentin Delorme <codelorme@gmail.com>
2018-09-16 20:32:30 +0200

    Update script python-manage.sh: Exit the script if the management task exited cleanly

--- scripts/python-manage.sh
+++ scripts/python-manage.sh
@@ -8,6 +8,11 @@ cd "${ROOTDIR}"
 
 while true; do
 	"${PYTHON}" manage.py $@
-	echo "${PYTHON} died! Restarting command `${PYTHON} manage.py $@` in ${SLEEP} second(s)"
-	sleep "${SLEEP}"
+	if [ ${?} -eq 0 ]; then
+		echo "Command \`${PYTHON} manage.py @@\` has quit, orderly exit (No challenge assigned to this Raspberry Pi)."
+		break
+	else
+		echo "${PYTHON} died! Restarting command \`${PYTHON} manage.py $@\` in ${SLEEP} second(s)"
+		sleep "${SLEEP}"
+	fi
 done
