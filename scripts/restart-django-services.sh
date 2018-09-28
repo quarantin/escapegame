db2e7cb28182115f9c2ddb4ca727c5c81e1a1017
Corentin Delorme <codelorme@gmail.com>
2018-09-28 13:37:03 +0200

    Update scripts: - Rename script django-service-start.sh => start-django-service.sh - Rename script django-service-stop.sh => stop-django-service.sh

--- scripts/restart-django-services.sh
+++ scripts/restart-django-services.sh
@@ -5,5 +5,5 @@
 ${ROOTDIR}/scripts/django-service-stop.sh
 
 for SERVICE in lift-control video-player monitor-network poll-gpios websocket-timer; do
-	${ROOTDIR}/scripts/django-service-start.sh ${SERVICE}
+	${ROOTDIR}/scripts/stop-django-service.sh ${SERVICE}
 done
