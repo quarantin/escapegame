9e28c4a4f1cc4f33de35b81a929ff16cd478e241
Corentin Delorme <codelorme@gmail.com>
2018-10-08 16:08:53 +0200

    Update script video-player.sh: Handle optional fourth parameter, if present, play file in a loop

--- scripts/video-player.sh
+++ scripts/video-player.sh
@@ -2,13 +2,13 @@
 
 . $(dirname $0)/env.sh
 
-if [ -z ${1} ] || [ -z ${2} ] || [ -z ${3} ]; then
-	echo "Usage: $0 <fifo> <audio-out> <url>"
+if [ -z "${1}" ] || [ -z "${2}" ] || [ -z "${3}" ]; then
+	echo "Usage: $0 <fifo> <audio-out> <url> [loop]"
 	exit
 fi
 
 FIFO=${1}
-if [ ! -p ${FIFO} ]; then
+if [ ! -p "${FIFO}" ]; then
 	mkfifo ${FIFO}
 fi
 
@@ -22,9 +22,13 @@ if [ ${RUNNING_ON_PI} = true ]; then
 	# the FIFO to start playing the video.
 	echo > ${FIFO} &
 
-	/usr/bin/omxplayer --no-osd --adev ${AUDIO_OUT} ${URL} < ${FIFO}
+	/usr/bin/omxplayer --no-osd --adev ${AUDIO_OUT} ${URL} ${4} < ${FIFO}
 else
-	/usr/bin/mpv --input-file ${FIFO} ${URL}
+	LOOP_PARAM='no'
+	if [ ! -z "${4}" ]; then
+		LOOP_PARAM='inf'
+	fi
+	/usr/bin/mpv --input-file=${FIFO} --loop-file=${LOOP_PARAM} "${URL}"
 fi
 
 rm -f ${FIFO}
