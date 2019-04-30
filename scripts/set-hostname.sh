3bafd68dd26f375e81d54839deb3672a455f004d
Corentin Delorme <codelorme@gmail.com>
2019-04-30 14:10:23 +0200

    Update scripts/set-hostname.sh: Make the script docker-friendly

--- scripts/set-hostname.sh
+++ scripts/set-hostname.sh
@@ -25,7 +25,9 @@ echo "[ * ] Configuring new hostname ${HOSTNAME} (current: ${OLD_HOSTNAME})"
 
 # Modify /etc/hosts so we can resolve both our old and new hostnames locally
 # This allow us to avoid the error sudo: Unable to resolve host <old hostname>
-sudo sed -i "s/^127.0.1.1[^0-9].*$/127.0.1.1\t${HOSTNAME} ${OLD_HOSTNAME}/" ${HOSTS_FILE}
+sudo cp ${HOSTS_FILE} ${HOSTS_FILE}.new
+sudo sed -i "s/^127.0.1.1[^0-9].*$/127.0.1.1\t${HOSTNAME} ${OLD_HOSTNAME}/" ${HOSTS_FILE}.new
+sudo mv ${HOSTS_FILE}.new ${HOSTS_FILE}
 
 # Modify /etc/hostname to reflect the new hostname
 echo -n "[ * ] Updating ${HOSTNAME_FILE} with new hostname \`${HOSTNAME}\`... "
@@ -39,10 +41,12 @@ echo OK
 
 # Reconfigure avahi-daemon for the change to take effect
 echo -n "[ * ] Restarting avahi-daemon for the change to take effect... "
-sudo dpkg-reconfigure avahi-daemon
+sudo -E dpkg-reconfigure avahi-daemon
 echo OK
 
 # Modify /etc/hosts to remove old hostname
 echo -n "[ * ] Updating ${HOSTS_FILE} with new hostname \`${HOSTNAME}\`... "
-sudo sed -i "s/^127.0.1.1[^0-9].*$/127.0.1.1\t$HOSTNAME/" ${HOSTS_FILE}
+sudo cp ${HOSTS_FILE} ${HOSTS_FILE}.new
+sudo sed -i "s/^127.0.1.1[^0-9].*$/127.0.1.1\t$HOSTNAME/" ${HOSTS_FILE}.new
+sudo mv ${HOSTS_FILE}.new ${HOSTS_FILE}
 echo OK
